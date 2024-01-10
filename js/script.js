97cc0e7d7131e99739720d1a7b7b54c394154f17
FernandoVerderame <fernandoverderame96@hotmail.com>
2024-01-10 17:47:53 +0100

    got button element and added click interaction

--- js/script.js
+++ js/script.js
@@ -2,8 +2,14 @@
 
 // Recuperiamo la griglia
 const grid = document.getElementById('grid');
+const button = document.querySelector('button');
 
 // Dati di partenza
 const rows = 10;
 const cols = 10;
 const totalCells = rows * cols; // 100
+
+//Creazione della griglia al click del bottone Play
+button.addEventListener('click', function() {
+
+})
