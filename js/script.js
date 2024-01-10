2ddaadc9e9c02c3c7466ac90d3d9dc56d0d1423c
FernandoVerderame <fernandoverderame96@hotmail.com>
2024-01-10 18:05:03 +0100

    added create cell function

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-// OPERAZIONI PRELIMINARI
+// ! OPERAZIONI PRELIMINARI
 
 // Recuperiamo la griglia
 const grid = document.getElementById('grid');
@@ -9,11 +9,26 @@ const rows = 10;
 const cols = 10;
 const totalCells = rows * cols; // 100
 
+
+// ! FUNZIONI
+const createCell = () => {
+    const newCell = document.createElement('div');
+    newCell.className = 'cell';
+    return newCell;
+}
+
+
+// ! EFFETTIVO SVOLGIMENTO DEL PROGRAMMA
 // Creazione della griglia al click del bottone Play
 button.addEventListener('click', function() {
 
     // Creazione ciclo for per ottenere la griglia
     for (let i = 0; i < totalCells; i++) {
-        
+
+        // Creare una cella
+        const cell = createCell();
+
+        // Aggiungo la cella in pagina
+        grid.appendChild(cell);
     }
 })
